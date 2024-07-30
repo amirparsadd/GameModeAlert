dc59f3ac506057bdc32aeb8f4454fd966957ed86
AmirparsaDD <Amirparsab9@gmail.com>
2024-07-30 15:33:06 +0330

    Code Improvementa

--- src/main/java/com/amirparsa/gamemodealert/GameModeChangeHandler.java
+++ src/main/java/com/amirparsa/gamemodealert/GameModeChangeHandler.java
@@ -13,13 +13,16 @@ public class GameModeChangeHandler implements Listener {
 
     @EventHandler
     public void onPlayerGameModeChange(PlayerGameModeChangeEvent event) {
-        if(!event.getPlayer().hasPermission("gamemodealert.bypass")){
-            if(GameModeAlert.getInstance().getConfig().getBoolean("console-log")) {
-                GameModeAlert.getInstance().getLogger().info(ChatColor.GREEN + "[" + GameModeAlert.getInstance().getConfig().getString("prefix", "GMA") + "] " + ChatColor.GRAY + "User " + event.getPlayer().getName() + "Changed His Game Mode To" + event.getNewGameMode().name());
-            }
-            for(Player p : Bukkit.getOnlinePlayers().stream().filter(p -> p.hasPermission("gamemodealert.admin")).collect(Collectors.toList())){
-                p.sendMessage(ChatColor.GREEN + "[" + GameModeAlert.getInstance().getConfig().getString("prefix", "GMA") + "] " + ChatColor.GRAY + "User " + event.getPlayer().getName() + "Changed His Game Mode To" + event.getNewGameMode().name());
-            }
+        if(event.getPlayer().hasPermission("gamemodealert.bypass")) return;
+
+        String message = ChatColor.GREEN + "[" + GameModeAlert.getInstance().getConfig().getString("prefix", "GMA") + "] " + ChatColor.GRAY + "User " + event.getPlayer().getName() + "Changed His Game Mode To" + event.getNewGameMode().name();
+
+        if(GameModeAlert.getInstance().getConfig().getBoolean("console-log")) {
+            GameModeAlert.getInstance().getLogger().info(message);
+        }
+
+        for(Player p : Bukkit.getOnlinePlayers().stream().filter(p -> p.hasPermission("gamemodealert.admin")).collect(Collectors.toList())){
+            p.sendMessage(message);
         }
     }
 }
